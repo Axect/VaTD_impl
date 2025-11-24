548ed8a8b5ef75a902ae8fef51023b733c62efa8
Axect <axect.tg@proton.me>
2025-11-24 15:20:52 +0800

    Model Start

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
+import torch
 from torch import nn
+import torch.nn.functional as F
 
 
 class MLP(nn.Module):
@@ -21,3 +23,215 @@ class MLP(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+
+
+class MaskedConv2D(nn.Conv2d):
+    def __init__(
+        self,
+        *args,
+        mask_type,
+        data_channels,
+        augment_channels=0,
+        augment_output=True,
+        **kwargs
+    ):
+        super(MaskedConv2D, self).__init__(*args, **kwargs)
+        assert mask_type in {"A", "B"}, "mask_type must be either 'A' or 'B'"
+
+        out_channels, in_channels, height, width = self.weight.size()
+        if augment_output:
+            assert (
+                in_channels % (data_channels + augment_channels) == 0
+                and out_channels % (data_channels + augment_channels) == 0
+            ), "When augment_output is True, in_channels and out_channels must be multiples of (data_channels + augment_channels)"
+        else:
+            assert (
+                in_channels % (data_channels + augment_channels) == 0
+                and out_channels % data_channels == 0
+            ), "When augment_output is False, in_channels must be a multiple of (data_channels + augment_channels) and out_channels must be a multiple of data_channels"
+        y_center, x_center = height // 2, width // 2
+
+        mask = torch.ones_like(self.weight)
+        mask[:, :, y_center + 1 :, :] = 0
+        mask[:, :, y_center, x_center:] = 0
+
+        if mask_type == "A":
+            meta_mask = torch.tril(
+                torch.ones((data_channels, data_channels)), diagonal=-1
+            )
+        else:
+            meta_mask = torch.tril(torch.ones((data_channels, data_channels)))
+
+        # For conditional channels
+        if augment_channels > 0:
+            if augment_output:
+                # Augment channel can't see data channel
+                meta_mask = torch.cat(
+                    [meta_mask, torch.zeros((augment_channels, data_channels))],
+                    dim=0,
+                )
+                # Data channel can see augment channel & augment channel can see itself
+                meta_mask = torch.cat(
+                    [
+                        meta_mask,
+                        torch.ones(
+                            (augment_channels + data_channels, augment_channels)
+                        ),
+                    ],
+                    dim=1,
+                )
+            else:
+                # Data channel can see augment channel & augment channel can see itself
+                meta_mask = torch.cat(
+                    [meta_mask, torch.ones((data_channels, augment_channels))],
+                    dim=1,
+                )
+
+        # Tiling meta mask to match real channels
+        in_tiles = in_channels // (data_channels + augment_channels)
+        if augment_output:
+            out_tiles = out_channels // (data_channels + augment_channels)
+        else:
+            out_tiles = out_channels // data_channels
+
+        # Caution: Original code has error here, fixed it.
+        mask[:, :, y_center, x_center] = meta_mask.repeat(out_tiles, in_tiles)
+
+        self.register_buffer("mask", mask)
+
+    def forward(self, x):
+        self.weight.data *= self.mask
+        return super(MaskedConv2D, self).forward(x)
+
+
+class MaskedResConv2D(nn.Module):
+    def __init__(
+        self,
+        channel,
+        kernel_size,
+        hidden_channels,
+        hidden_conv_layers,
+        hidden_kernel_size,
+        hidden_width,
+        hidden_fc_layers,
+        category,
+        augment_channels=0,
+    ):
+
+        super().__init__()
+
+        self.channel = channel
+        self.category = category
+        self.first_conv = MaskedConv2D(
+            in_channels=channel + augment_channels,
+            out_channels=2 * hidden_channels,
+            kernel_size=kernel_size,
+            padding=(kernel_size - 1) // 2,
+            mask_type="A",
+            data_channels=channel,
+            augment_channels=augment_channels,
+            augment_output=True,
+        )
+        hidden_convs = []
+        for _ in range(hidden_conv_layers):
+            hidden_convs.append(
+                nn.Sequential(
+                    MaskedConv2D(
+                        in_channels=2 * hidden_channels,
+                        out_channels=hidden_channels,
+                        kernel_size=1,
+                        padding=(hidden_kernel_size - 1) // 2,
+                        mask_type="B",
+                        data_channels=channel,
+                        augment_channels=augment_channels,
+                        augment_output=True,
+                    ),
+                    nn.GELU(),
+                    MaskedConv2D(
+                        in_channels=hidden_channels,
+                        out_channels=hidden_channels,
+                        kernel_size=hidden_kernel_size,
+                        padding=(hidden_kernel_size - 1) // 2,
+                        mask_type="B",
+                        data_channels=channel,
+                        augment_channels=augment_channels,
+                        augment_output=True,
+                    ),
+                    nn.GELU(),
+                    MaskedConv2D(
+                        in_channels=hidden_channels,
+                        out_channels=2 * hidden_channels,
+                        kernel_size=1,
+                        mask_type="B",
+                        data_channels=channel,
+                        augment_channels=augment_channels,
+                        augment_output=True,
+                    ),
+                )
+            )
+        self.hidden_convs = nn.ModuleList(hidden_convs)
+        self.first_fc = MaskedConv2D(
+            in_channels=2 * hidden_channels,
+            out_channels=hidden_width,
+            kernel_size=1,
+            mask_type="B",
+            data_channels=channel,
+            augment_channels=augment_channels,
+            augment_output=True,
+        )
+
+        hidden_fcs = []
+        for _ in range(hidden_fc_layers):
+            hidden_fcs.append(
+                MaskedConv2D(
+                    in_channels=hidden_width,
+                    out_channels=hidden_width,
+                    kernel_size=1,
+                    mask_type="B",
+                    data_channels=channel,
+                    augment_channels=augment_channels,
+                    augment_output=True,
+                )
+            )
+        self.hidden_fcs = nn.ModuleList(hidden_fcs)
+
+        self.final_fc = MaskedConv2D(
+            in_channels=hidden_width,
+            out_channels=category * channel,
+            kernel_size=1,
+            mask_type="B",
+            data_channels=channel,
+            augment_channels=augment_channels,
+            augment_output=False,
+        )
+
+    def forward(self, x):
+        size = x.shape
+        x = self.first_conv(x)
+        x = F.gelu(x)
+
+        for layer in self.hidden_convs:
+            tmp = layer(x)
+            tmp = F.gelu(tmp)
+            x = x + tmp
+
+        x = self.first_fc(x)
+        x = F.gelu(x)
+
+        for layer in self.hidden_fcs:
+            x = layer(x)
+            x = F.gelu(x)
+
+        x = self.final_fc(x)
+
+        return x.reshape(size[0], self.channel, self.category, size[-2], size[-1])
+
+
+class DiscretePixelCNN(nn.Module):
+    def __init__(self, hparams, device="cpu"):
+        super(DiscretePixelCNN, self).__init__()
+        self.hparams = hparams
+        self.device = device
+
+        self.channel = 1
+        self.augment_channels = 1
