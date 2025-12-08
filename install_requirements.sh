d52d77cc007bf901487329cf08144ff8fc9c4f74
Axect <axect.tg@proton.me>
2025-12-08 22:26:10 +0800

    Add nflows

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,1 +1,1 @@
-uv pip install -U torch wandb rich beaupy fireducks numpy optuna matplotlib scienceplots einops
+uv pip install -U torch wandb rich beaupy fireducks numpy optuna matplotlib scienceplots einops nflows
