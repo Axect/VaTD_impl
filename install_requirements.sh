a6d23a36c76e182a2e0456984de16042ce1d5e0f
axect <axect.tg@proton.me>
2026-03-05 00:46:05 +0800

    fla

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,1 +1,1 @@
-uv pip install -U torch wandb rich beaupy fireducks numpy optuna matplotlib scienceplots einops nflows
+uv pip install -U torch wandb rich beaupy fireducks numpy optuna matplotlib scienceplots einops nflows flash-linear-attention
